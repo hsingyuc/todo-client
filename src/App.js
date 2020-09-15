6d4e24d2517f8fa0536aa50fbc789ac8d56625dc
hsing <hsingyuc.7@gmail.com>
2020-09-15 19:56:55 -0400

    Fetch todos from database.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  getPosts() {
+    fetch('http://localhost:3000/todos')
+      .then( response => response.json() )
+      .then( json => this.updateTodos(json) )
+  }
+
   render() {
     return null;
   }
