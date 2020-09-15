5bbb6be162bd808dd8e5a10506d841cbd2486397
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-09-15 19:02:02 -0400

    Store - Create state, actions, and reducer. (#1)
    * Define actions.
    
    * Create reducer.

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,48 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-export default configureStore({
-  reducer: {
-    // counter: counterReducer,
-  },
+const initialState = {
+  todos:[],
+  editingTodoId: null
+};
+
+export const setEditingId = id => ({
+  type: 'SET_EDITING_ID',
+  payload: id
 });
+
+export const addTodo = todo => ({
+  type: 'ADD_TODO',
+  payload: todo
+});
+
+export const removeTodo = id => ({
+  type: 'REMOVE_TODO',
+  payload: id
+});
+
+const reducer = ( state = initialState, action ) => {
+  switch(action.type) {
+    case 'SET_EDITING_ID':
+      return {
+        ...state,
+        editingTodoId: action.payload
+      };
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [ ...state.todos, action.payload ]
+      };
+    case 'REMOVE_TODO':
+      const newTodos = [...state.todos];
+      const index = newTodos.findIndex( t => t.id === action.id );
+      newTodos.splice( index, 1 );
+      return {
+        ...state,
+        todos: newTodos
+      };
+    default:
+      return state;
+  }
+};
+
+export default configureStore(reducer);
