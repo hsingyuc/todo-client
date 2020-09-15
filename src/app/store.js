2510575d5318b566074e60a82fb98b997f8f2fbe
hsing <hsingyuc.7@gmail.com>
2020-09-15 18:15:21 -0400

    Define actions.

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-export default configureStore({
-  reducer: {
-    // counter: counterReducer,
-  },
+const initialState = {
+  todos:[],
+  editingTodoId: null
+};
+
+const setEditingId = id => ({
+  type: 'SET_EDITING_ID',
+  payload: id
 });
+
+const addTodo = todo => ({
+  type: 'ADD_TODO',
+  payload: todo
+});
+
+const removeTodo = id => ({
+  type: 'REMOVE_TODO',
+  payload: id
+});
+
+
+
+export default configureStore;
