774a09862e59a844fd1cb2c359df98adb77f99fc
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-09-21 19:36:17 -0400

    Add/todos (#5)
    * Add todos component.
    
    * Add router on app.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { setEditingId, setTodos } from './app/store';
+import { setEditingId, setTodos as setTodosAction } from './app/store';
 import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home';
+import Todos from './Todos';
 
 class App extends React.Component {
+  componentDidMount() {
+    this.getPosts();
+  }
+
   getPosts() {
     fetch('http://localhost:3000/todos')
       .then( response => response.json() )
@@ -16,7 +28,22 @@ class App extends React.Component {
   }
 
   render() {
-    return null;
+    return (
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/todos">Todos</Link>
+
+    {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/todos">
+          <Todos />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>);
   }
 }
 
@@ -27,7 +54,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
 	editId: id => dispatch(setEditingId(id)),
-	setTodos: todos => dispatch(setTodos(todos)),
+	setTodos: todos => dispatch(setTodosAction(todos)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
