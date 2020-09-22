d142c1013aa28f598f6bc31b8f15b87ece401566
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-09-22 18:44:36 -0400

    Add createTodo and category components (#12)
    * Install ant design.
    
    * Add category and createTodo components.

---
+++ src/CreateTodo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Category from './Category';
+
+export default class CreateTodo extends React.PureComponent {
+	render() {
+		return(
+			<Category />
+		);
+	}
+}
