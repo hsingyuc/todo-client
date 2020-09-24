482812dec5af5077cc55b6a6b980c135b0b450a8
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-09-24 18:37:49 -0400

    Remove redux addTodoAction. (#15)

--- src/Category.js
+++ src/Category.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Menu } from 'antd';
 import 'antd/dist/antd.css';
-import { connect } from 'react-redux';
-import { addTodo as addTodoAction } from './app/store';
 
-class Category extends React.Component {
+export default class Category extends React.Component {
 	constructor( props) {
 		super( props );
 		this.state = {
@@ -12,7 +10,7 @@ class Category extends React.Component {
 		}
 	}
 
-	handleCategoryClick(category) {
+	handleCategoryClick( category ) {
 		this.setState( { category } );
 	}
 
@@ -20,27 +18,21 @@ class Category extends React.Component {
 		const { category } = this.state;
 
 		return(
-		<Menu onClick={this.handleClick} selectedKeys={[category]} mode="horizontal">
-			<Menu.Item key="productive" onClick={ () => this.handleCategoryClick('productive')}>
+		<Menu onClick={this.handleClick} selectedKeys={[ category ]} mode="horizontal">
+			<Menu.Item key="productive" onClick={ () => this.handleCategoryClick('productive') }>
 				Productive
 			</Menu.Item>
-			<Menu.Item key="routine" onClick={ () => this.handleCategoryClick('routine')}>
+			<Menu.Item key="routine" onClick={ () => this.handleCategoryClick('routine') }>
 				Routine
 			</Menu.Item>
 			<Menu.SubMenu key="others" title="Others">
-				<Menu.Item key="meeting" onClick={ () => this.handleCategoryClick('meeting')}>Meeting</Menu.Item>
-				<Menu.Item key="event" onClick={ () => this.handleCategoryClick('event')}>Event</Menu.Item>
-				<Menu.Item key="climbing" onClick={ () => this.handleCategoryClick('climbing')}>Climbing</Menu.Item>
-				<Menu.Item key="gym" onClick={ () => this.handleCategoryClick('gym')}>Gym</Menu.Item>
-				<Menu.Item key="grocery" onClick={ () => this.handleCategoryClick('grocery')}>Grocery</Menu.Item>
+				<Menu.Item key="meeting" onClick={ () => this.handleCategoryClick('meeting') }>Meeting</Menu.Item>
+				<Menu.Item key="event" onClick={ () => this.handleCategoryClick('event') }>Event</Menu.Item>
+				<Menu.Item key="climbing" onClick={ () => this.handleCategoryClick('climbing') }>Climbing</Menu.Item>
+				<Menu.Item key="gym" onClick={ () => this.handleCategoryClick('gym') }>Gym</Menu.Item>
+				<Menu.Item key="grocery" onClick={ () => this.handleCategoryClick('grocery') }>Grocery</Menu.Item>
 			</Menu.SubMenu>
 		</Menu>
 		);
 	}
 }
-
-const mapDispatchToProps = dispatch => ({
-	addTodo: todo => dispatch(addTodoAction(todo)),
-});
-
-export default connect(null, mapDispatchToProps)(Category);
