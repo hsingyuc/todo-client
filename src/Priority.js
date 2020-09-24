c73558687e9d64972ede54c65246bb3f3b9af7b1
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-09-24 18:27:04 -0400

    Add priority buttons for creat todo. (#14)

---
+++ src/Priority.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Menu } from 'antd';
+import 'antd/dist/antd.css';
+
+export default class Priority extends React.Component {
+	constructor( props) {
+		super( props );
+		this.state = {
+			priority: null
+		}
+	}
+
+	handlePriorityClick( priority ) {
+		this.setState( { priority } );
+	}
+
+	render() {
+		const { priority } = this.state;
+
+		return(
+		<Menu onClick={this.handleClick} selectedKeys={[ priority ]} mode="horizontal">
+			<Menu.Item key="primary" onClick={ () => this.handlePriorityClick('primary') }>
+				Primary
+			</Menu.Item>
+			<Menu.Item key="secondary" onClick={ () => this.handlePriorityClick('secondary') }>
+				Routine
+			</Menu.Item>
+			<Menu.Item key="tertiary" onClick={ () => this.handlePriorityClick('tertiary') }>
+				Tertiary
+			</Menu.Item>
+		</Menu>
+		);
+	}
+}
