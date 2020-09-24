02029936cf37e1499fadb3580a145251c7aea3a9
hsing <hsingyuc.7@gmail.com>
2020-09-24 18:20:19 -0400

    Add priority buttons for creat todo.

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Category from './Category';
+import Priority from './Priority';
 
 export default class CreateTodo extends React.PureComponent {
 	render() {
 		return(
-			<Category />
+			<>
+				<Category />
+				<Priority />
+			</>
 		);
 	}
 }
