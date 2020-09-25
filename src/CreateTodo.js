941975b47beb86da7e23a4d2374b66068e0fa519
hsing <hsingyuc.7@gmail.com>
2020-09-25 17:15:27 -0400

    Add space component for styling.

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Space } from 'antd';
 import Category from './Category';
 import Priority from './Priority';
 import Task from './Task';
-import StartAndEndDatePicker from './StartAndEndDatePicker';
+import DateAndTimePicker from './DateAndTimePicker';
 
 export default class CreateTodo extends React.PureComponent {
 	render() {
 		return(
-			<>
+			<Space direction="vertical">
 				<Category />
 				<Priority />
 				<Task />
-				<StartAndEndDatePicker />
-			</>
+				<DateAndTimePicker />
+			</Space>
 		);
 	}
 }
