dbeceb5079c757da874bba0792ace07060ee31aa
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-10-13 20:52:59 -0400

    Add/todo (#27)
    * Redirect to show one specific todo.
    
    * Add the route.
    
    * Add image source for attachment.

---
+++ src/Todo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from '@reduxjs/toolkit';
+
+class Todo extends React.Component {
+	render() {
+		const { todos, match } = this.props;
+		const todo = todos.find( t => t.id === parseInt(match.params.id));
+
+		if( !todo ) {
+			return 'Loading...';
+		}
+		return(
+			<div>
+				<Link to="/">
+					<button type="button">
+						Home
+					</button>
+				</Link>
+
+				<div>
+					<h1>{todo.task}</h1>
+					<p>Category: {todo.category}</p>
+					<p>Priority: {todo.priority}</p>
+					<p>Start time: {todo.startTime}</p>
+					<p>End time: {todo.endTime}</p>
+					<p>Attachment:</p> 
+					<img src={`http://localhost:3000/uploads/${todo.attachment}`} alt='attachment' />
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProp = state => ({
+	todos: state.todos,
+	editingId: state.editingId
+});
+
+export default compose(
+	withRouter,
+	connect(mapStateToProp)
+)(Todo);
