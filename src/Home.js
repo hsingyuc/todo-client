3d9567fe81312437082095292d0dcc16b72b8129
hsing <hsingyuc.7@gmail.com>
2020-10-19 14:18:21 -0400

    Add calendar on home page render.

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Calendar, Badge } from 'antd';
 
-class Home extends React.Component {
+export default class Home extends React.Component {
+	constructor( props ) {
+		super( props );
+		this.dateCellRender = this.dateCellRender.bind( this );
+	}
+
+	getListData(value) {
+		let listData;
+		switch (value.date()) {
+		  case 8:
+			listData = [
+			  { type: 'warning', content: 'This is warning event.' },
+			  { type: 'success', content: 'This is usual event.' },
+			];
+			break;
+		  case 10:
+			listData = [
+			  { type: 'warning', content: 'This is warning event.' },
+			  { type: 'success', content: 'This is usual event.' },
+			  { type: 'error', content: 'This is error event.' },
+			];
+			break;
+		  case 15:
+			listData = [
+			  { type: 'warning', content: 'This is warning event' },
+			  { type: 'success', content: 'This is very long usual event。。....' },
+			  { type: 'error', content: 'This is error event 1.' },
+			  { type: 'error', content: 'This is error event 2.' },
+			  { type: 'error', content: 'This is error event 3.' },
+			  { type: 'error', content: 'This is error event 4.' },
+			];
+			break;
+		  default:
+		}
+		return listData || [];
+	}
+
+	dateCellRender(value) {
+		const listData = this.getListData(value);
+		return (
+		  <ul className="events">
+			{listData.map(item => (
+			  <li key={item.content}>
+				<Badge status={item.type} text={item.content} />
+			  </li>
+			))}
+		  </ul>
+		);
+	}
+
 	render() {
 		return(
 			<div>
-				<p>Hello</p>
+				<Calendar dateCellRender={this.dateCellRender} />
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = state => ({
-	todos: state.todos,
-	editingId: state.editingId
-  });
-  
-  export default connect(mapStateToProps)(Home);
