32ba9f38a37a6b9d570d211e2cc07f6082ef1189
hsing <hsingyuc.7@gmail.com>
2020-10-19 17:49:27 -0400

    Filter todo to match dates.

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,41 @@
 import React from 'react';
 import { Calendar, Badge } from 'antd';
+import { connect } from 'react-redux';
 
-export default class Home extends React.Component {
+class Home extends React.Component {
 	constructor( props ) {
 		super( props );
-		this.dateCellRender = this.dateCellRender.bind( this );
+		this.dateCellRender = this.dateCellRender.bind(this);
 	}
 
-	getListData(value) {
-		let listData;
-		switch (value.date()) {
-		  case 8:
-			listData = [
-			  { type: 'warning', content: 'This is warning event.' },
-			  { type: 'success', content: 'This is usual event.' },
-			];
-			break;
-		  case 10:
-			listData = [
-			  { type: 'warning', content: 'This is warning event.' },
-			  { type: 'success', content: 'This is usual event.' },
-			  { type: 'error', content: 'This is error event.' },
-			];
-			break;
-		  case 15:
-			listData = [
-			  { type: 'warning', content: 'This is warning event' },
-			  { type: 'success', content: 'This is very long usual event。。....' },
-			  { type: 'error', content: 'This is error event 1.' },
-			  { type: 'error', content: 'This is error event 2.' },
-			  { type: 'error', content: 'This is error event 3.' },
-			  { type: 'error', content: 'This is error event 4.' },
-			];
-			break;
-		  default:
+	getTodosForDate(date) {
+		const startOfDay = date.startOf('day').unix();
+		const endOfDay = date.endOf('day').unix();
+		const { todos } = this.props;
+		
+		if ( !todos ) {
+			alert('No todos!');
 		}
-		return listData || [];
+		return todos.filter( todo => {
+			return ( todo.startTime >= startOfDay && todo.startTime <= endOfDay ) ||
+			( todo.endTime <= endOfDay && todo.endTime >= startOfDay ) ||
+			( startOfDay >= todo.startTime && startOfDay <= todo.endTime );
+		} );
+		
 	}
+	
+	
 
-	dateCellRender(value) {
-		const listData = this.getListData(value);
+	dateCellRender(date) {
+		const listData = this.getTodosForDate(date);
 		return (
-		  <ul className="events">
+		  <div>
 			{listData.map(item => (
-			  <li key={item.content}>
-				<Badge status={item.type} text={item.content} />
-			  </li>
+			  <span key={item.id}>
+				<Badge />
+			  </span>
 			))}
-		  </ul>
+		  </div>
 		);
 	}
 
@@ -60,3 +48,8 @@ export default class Home extends React.Component {
 	}
 }
 
+const mapStateToProps = state => ({
+	todos: state.todos
+})
+
+export default connect(mapStateToProps, null)(Home);
