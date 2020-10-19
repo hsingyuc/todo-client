0372ecd8f5de4efb3ea27122dea17aa38e7e7610
Chang Hsing-Yu <56378160+hsingyuc@users.noreply.github.com>
2020-10-19 18:03:39 -0400

    Add/home calendar (#30)
    * Add calendar on home page render.
    
    * Filter todo to match dates.
    
    * Match priority to badge status color.

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,65 @@
 import React from 'react';
+import { Calendar, Badge } from 'antd';
 import { connect } from 'react-redux';
 
 class Home extends React.Component {
+	constructor( props ) {
+		super( props );
+		this.dateCellRender = this.dateCellRender.bind(this);
+	}
+
+	getTodosForDate(date) {
+		const startOfDay = date.startOf('day').unix();
+		const endOfDay = date.endOf('day').unix();
+		const { todos } = this.props;
+		
+		if ( !todos ) {
+			alert('No todos!');
+		}
+		return todos.filter( todo => {
+			return ( todo.startTime >= startOfDay && todo.startTime <= endOfDay ) ||
+			( todo.endTime <= endOfDay && todo.endTime >= startOfDay ) ||
+			( startOfDay >= todo.startTime && startOfDay <= todo.endTime );
+		} );
+		
+	}
+	
+	getBadgeStatus( priority ) {
+		if ( 'primary' === priority )  {
+			return 'error';
+		}
+		if ( 'secondary' === priority )  {
+			return 'warning';
+		}
+		if ( 'tertiary' === priority )  {
+			return 'success';
+		}
+	}
+
+	dateCellRender(date) {
+		const listData = this.getTodosForDate(date);
+		return (
+		  <div>
+			{listData.map(item => (
+			  <span key={item.id}>
+				<Badge status={this.getBadgeStatus(item.priority)}/>
+			  </span>
+			))}
+		  </div>
+		);
+	}
+
 	render() {
 		return(
 			<div>
-				<p>Hello</p>
+				<Calendar dateCellRender={this.dateCellRender} />
 			</div>
 		);
 	}
 }
 
 const mapStateToProps = state => ({
-	todos: state.todos,
-	editingId: state.editingId
-  });
-  
-  export default connect(mapStateToProps)(Home);
+	todos: state.todos
+})
+
+export default connect(mapStateToProps, null)(Home);
