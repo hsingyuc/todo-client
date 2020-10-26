fc1ffe6c438db9d7f13bd774b8d1478743f5141a
hsing <hsingyuc.7@gmail.com>
2020-10-26 16:45:34 -0400

    Create todosMap to assign property:todo start time and value:todos.

--- src/Todos.js
+++ src/Todos.js
@@ -1,7 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import * as moment from 'moment';
 
 export default class Todos extends React.PureComponent {
+	renderTimeline(todos) {
+	//@Todo1 Create todosMap
+		// eg. todosMap = {
+				// 	0: todo1,
+				// 	1: [
+					// 		todo1.1,
+					// 		todo1.2
+					// 	],
+				// 	2: todo3
+				// };  
+		// @Todo1.1 Create todoStartHour property.
+		// @Todo1.2 Assign todo.task to todosMap.todoStartHour
+		
+		const todosMap = {};
+
+		todos.map( todo => {
+			const todoStartHour = moment.unix(todo.startTime).format('H');
+			
+			if( !todosMap[ todoStartHour ] ) {
+				todosMap[ todoStartHour ] = [];
+			}
+			todosMap[ todoStartHour ].push( todo );
+		} );
+
+		// @Todo2 loop over timeLine
+			// <Timeline mode={'left'}>
+			// 	<Timeline.Item label='timeLine'>
+			// 		<Link to={`/todos/${todo.id}`}>
+			// 			{todo.task}
+			// 		</Link>
+			// 	</Timeline.Item>
+			// </Timeline>
+		
+		console.log(todosMap);
+	}
+
 	render() {
 		const { todos } = this.props;
 	
@@ -14,20 +50,7 @@ export default class Todos extends React.PureComponent {
 		}
 		return(
 			<div>
-				{todos.map( todo => {
-					return(
-						<div key={todo.id}>
-							<h1>
-								<Link to={`/todos/${todo.id}`}>
-									{todo.task}
-								</Link>
-							</h1>
-							{todo.startTime}
-							{todo.endTime}
-						</div>
-					);
-				}
-				)}
+				{this.renderTimeline(todos)}
 			</div>
 		);
 	}
